c4facd434732c106ab77a704ec6e166ff8bf14f9
dimanator13 <dimadimich0@gmail.com>
2026-05-27 21:38:47 +0400

    Add Avalonia task

---
+++ Task_7/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Task_7.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Task_7/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Task_7.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
